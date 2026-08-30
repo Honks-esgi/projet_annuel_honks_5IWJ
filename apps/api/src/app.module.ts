49014aeb5bf1f047bdf13fc38f9b70f983e98d4e
Brubru <jordan.sankare@gmail.com>
2026-08-30 14:37:28 +0200

    need push for docker img rebuild w/ github workflow

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [PrometheusModule.register()],
+  imports: [
+      PrometheusModule.register(),
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT ?? '5432'),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
